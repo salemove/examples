390dce90093b56500bd9a130e8ab6400672f7263
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2019-08-21 01:33:12 +0300

    Disable RSpec warnings
    They're very noisy about dependencies, that we can't really affect.

--- rest_api/ruby/wfm_integration/spec/spec_helper.rb
+++ rest_api/ruby/wfm_integration/spec/spec_helper.rb
@@ -14,7 +14,6 @@ RSpec.configure do |config|
   config.filter_run_when_matching :focus
   config.example_status_persistence_file_path = 'spec/examples.txt'
   config.disable_monkey_patching!
-  config.warnings = true
   config.default_formatter = 'doc' if config.files_to_run.one?
   config.profile_examples = 10
   config.order = :random
