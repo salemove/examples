2c005aa2b653ebde2fa5cd85f8bf376a09f5ecac
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2019-08-21 13:43:17 +0300

    Add first integration test for AgentData
    This gives at least a basic guarantee against regressions when making changes
    to code.
    
    Authorization is set up in a way, that when new tests are added, then the
    should be first run with the `TOKEN` environment variable set. VCR will then
    record the requests and responses, masking the authorization header, and
    subsequent test runs don't need the `TOKEN` environment variable to be set
    anymore.

--- rest_api/ruby/wfm_integration/spec/agent_data_spec.rb
+++ rest_api/ruby/wfm_integration/spec/agent_data_spec.rb
@@ -1,10 +1,47 @@
 # frozen_string_literal: true
 
 require 'agent_data'
+require 'ostruct'
 
-RSpec.describe AgentData do
-  it 'works' do
-    a = true
-    expect(a).to eq(true)
+RSpec.describe AgentData, :vcr do
+  it 'returns agent data' do
+    expect(agent_data.call.values).to eq([{
+      admin_dur: 0,
+      agent_id: '9a5e3745-c5ed-4818-ba4c-4d52a851027c',
+      agent_name: 'WFM Integration Example Operator',
+      avail_dur: 1950,
+      direct_in_call_cnt: 3,
+      direct_in_call_dur: 53,
+      direct_out_call_cnt: 3,
+      direct_out_call_dur: 106,
+      pause_dur: 6,
+      site_id: '7e8aea41-f985-473f-83e3-fba01631acdb',
+      tot_work_dur: 1956,
+      wait_dur: 1791
+    }, {
+      admin_dur: 874,
+      agent_id: 'e18c24e1-f753-4911-a695-801d2e5279dd',
+      agent_name: 'WFM Integration Example Manager',
+      avail_dur: 1661,
+      direct_in_call_cnt: 1,
+      direct_in_call_dur: 22,
+      direct_out_call_cnt: 3,
+      direct_out_call_dur: 133,
+      pause_dur: 874,
+      site_id: '7e8aea41-f985-473f-83e3-fba01631acdb',
+      tot_work_dur: 2535,
+      wait_dur: 1153
+    }])
+  end
+
+  def agent_data
+    opts = OpenStruct.new
+    opts.api = 'https://api.salemove.com'
+    opts.start_date = '2019-08-21T17:00:00Z'
+    opts.end_date = '2019-08-21T18:00:00Z'
+    opts.site_id = ['7e8aea41-f985-473f-83e3-fba01631acdb']
+    opts.token = ENV['TOKEN']
+
+    described_class.new(opts)
   end
 end
