52ab02f7cb3c0b8c74fbe37380efaeaea4958b59
erxx99 <erki99@gmail.com>
2020-03-17 17:18:46 +0200

    Fix code to follow best practices

--- js_sdk/install_glia_with_site_id.js
+++ js_sdk/install_glia_with_site_id.js
@@ -1,17 +1,18 @@
 var installGlia = function(siteId, callback) {
-  const gliaIntegrationScriptUrl = `https://api.salemove.com/salemove_integration.js?site_id=${siteId}`;
-  const scriptTagName = 'script';
-  var scriptElement = document.createElement(scriptTagName);
+  var gliaIntegrationScriptUrl = 'https://api.salemove.com/salemove_integration.js?site_id=' + siteId;
+  var scriptElement = document.createElement('script');
 
   scriptElement.async = 1;
   scriptElement.src = gliaIntegrationScriptUrl;
-  scriptElement.type = "text/javascript";
-  scriptElement.addEventListener("load", callback);
+  scriptElement.type = 'text/javascript';
+  if (typeof(callback) === 'function') {
+    scriptElement.addEventListener('load', callback);
+  }
   
   document.body.append(scriptElement);
 };
 
 
-installGlia("11111111-2222-3333-4444-555555555555", function(){
+installGlia('11111111-2222-3333-4444-555555555555', function(){
   // Any post install configuration
 });
