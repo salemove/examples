49eef7cb3bb809622835b1256ee689d41c0bc485
erxx99 <erki99@gmail.com>
2020-03-19 10:24:04 +0200

    Add clearer comments about site_id UUID

--- js_sdk/install_glia_with_site_id.js
+++ js_sdk/install_glia_with_site_id.js
@@ -12,7 +12,12 @@ var installGlia = function(siteId, callback) {
   document.body.append(scriptElement);
 };
 
-var siteId = '11111111-2222-3333-4444-555555555555'; // This can be requested from your Client Success Manager
+var siteId = '11111111-2222-3333-4444-555555555555'; 
+/* 
+This is a UUID for your site that needs to be requested from your Client Success Manager 
+or via our Service Desk https://salemove.atlassian.net/servicedesk/customer/portal/1 
+*/
+
 installGlia(siteId, function(){
   // Any post install configuration
 });
